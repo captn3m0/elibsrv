fa93643626009ee132088b739b09232eed9fbc6c
mv_fox <mv_fox@a7b99cd1-0d9e-4bb3-a6bd-278b8191f679>
2015-12-18 00:08:11 +0000

    optimized html output for small screens
    git-svn-id: https://svn.code.sf.net/p/elibsrv/code@84 a7b99cd1-0d9e-4bb3-a6bd-278b8191f679

--- frontend/out_html.php
+++ frontend/out_html.php
@@ -61,8 +61,9 @@ function printheaders_html(array $pageinfo) {
   echo "<head>\n";
   echo "  <title>" . htmlentities($pageinfo['title']) . "</title>\n";
   echo '  <link rel="shortcut icon" href="favicon.png">' . "\n";
+  echo '  <meta name="viewport" content="width=device-width">' . "\n";
   echo "  <style>\n";
-  echo '    body { font-family: "Open Sans", "Helvetica", sans-serif; min-width: 20em;}' . "\n";
+  echo '    body { font-family: "Open Sans", "Helvetica", sans-serif; min-width: 0; }' . "\n";
   echo "    p { padding: 0 1em 0 1em; margin: 0.1em 0 0.1em 0; }\n";
   echo "    p.menutitle { background-color: #fdc; font-weight: bold; margin-bottom: 0.9em; }\n";
   echo "    p.acqimg { margin: 0.5em 0 0 0; padding: 0; }\n";
