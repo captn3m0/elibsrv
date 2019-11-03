90d8e62c773723d9bdd67cfee6d30cf776bc56c1
Nemo <me@captnemo.in>
2019-11-04 00:51:12 +0530

    Add support for running in scan/serve/scan+serve mode
    - Default is scan+serve, same as previous

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,26 @@ sigil -f /etc/elibsrv.conf.tmpl -p > /etc/elibsrv.conf
 mkdir --parents /books /config /cache
 chmod 777 /cache
 
-# Run a one-time scan on /books
-echo "[+] Scanning EPUB files on first launch"
-find /books/ -regextype posix-egrep -iregex '.*\.((epub)|(pdf))' | elibsrv -v /etc/elibsrv.conf
+echo "HELKLO=$1"
 
-echo "[+] Starting web server"
-apachectl -D FOREGROUND
+case "$1" in
+    "scan" )
+        SCAN=1 ;;
+    "serve" )
+        SERVE=1 ;;
+    *)
+        SCAN=1
+        SERVE=1
+esac
+
+echo $SCAN $SERVE
+
+if [[ "$SCAN" = "1" ]]; then
+    echo "[+] Scanning EPUB files"
+    find /books/ -regextype posix-egrep -iregex '.*\.((epub)|(pdf))' | elibsrv -v /etc/elibsrv.config
+fi
+
+if [[ "$SERVE" = "1" ]]; then
+    echo "[+] Starting web server"
+    apachectl -D FOREGROUND
+fi
